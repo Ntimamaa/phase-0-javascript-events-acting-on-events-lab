c64d0c1eab85757d449da34dfbe1281db6330042
Drake Tempes <dreake.ntimama@student.moringaschool.com>
2023-06-11 18:50:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style .left = `${left - 1}px`;
+    }
+  }
+  
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+function moveDodgerRight() {
+  const rightNumbers = dodger.style.left.replace("px", "");
+  let left= parseInt(rightNumbers, 10);
+
+  if (left < 360) {
+    left += 1;
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (a) {
+  if (a.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
+
+  
+  
